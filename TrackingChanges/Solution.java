4f42378ec92e4b7e33f7a1184a6f79dd451e251a
hanza <hanza@hanzas-Mac-mini.local>
2021-05-17 17:25:01 +0300

    Add TrackingChanges Proj

--- TrackingChanges/Solution.java
+++ TrackingChanges/Solution.java
@@ -7,6 +7,39 @@ import java.util.List;
 
 /* 
 Отслеживаем изменения
+
+Считать с консоли 2 имени файла - file1, file2.
+Файлы содержат строки, file2 является обновленной версией file1, часть строк совпадают.
+Нужно создать объединенную версию строк, записать их в список lines.
+Операции ADDED и REMOVED не могут идти подряд, они всегда разделены SAME.
+Пустые строки даны в примере для наглядности.
+В оригинальном и редактируемом файлах пустых строк нет!
+
+Пример 1:
+оригинальный    редактированный    общий
+file1:          file2:             результат:(lines)
+ 
+строка1         строка1            SAME строка1
+строка2                            REMOVED строка2
+строка3         строка3            SAME строка3
+строка4                            REMOVED строка4
+строка5         строка5            SAME строка5
+                строка0            ADDED строка0
+строка1         строка1            SAME строка1
+строка2                            REMOVED строка2
+строка3         строка3            SAME строка3
+                строка4            ADDED строка4
+строка5         строка5            SAME строка5
+строка0                            REMOVED строка0
+
+Пример 2:
+оригинальный    редактированный    общий
+file1:          file2:             результат:(lines)
+ 
+строка1         строка1            SAME строка1
+                строка0            ADDED строка0
+
+Пустые строки в примере означают, что этой строки нет в определенном файле.
 */
 
 public class Solution {
@@ -26,18 +59,57 @@ public class Solution {
             while(editedFileReader.ready()){
                 editedFile.add(editedFileReader.readLine());
             }
+            
+            int i = 0;
+            int j = 0;
+            while(i < originFile.size() && j < editedFile.size()){
+                if(!originFile.get(i).isEmpty() && !editedFile.get(j).isEmpty()){
+                    if(originFile.get(i).equals(editedFile.get(j))){
+                    //если совпадают строки
+                        lines.add(new LineItem(Type.SAME, originFile.get(i)));
+                    }else{
+                    //если строки в оригинальном и новом файлах не равны
+                        if(j+1 < editedFile.size()){
+                            if(editedFile.get(j + 1).equals(originFile.get(i))){
+                            //если совпадают строки
+                               lines.add(new LineItem(Type.SAME, originFile.get(i)));
+                                lines.add(new LineItem(Type.ADDED, editedFile.get(j)));
+                                j+=1;
+                            }else{
+                                lines.add(new LineItem(Type.REMOVED, originFile.get(i)));
+                                lines.add(new LineItem(Type.ADDED, editedFile.get(j)));
+                            }
+                        }
+                    }
+                }else if(!originFile.get(i).isEmpty() && editedFile.get(j).isEmpty()){
+                    //если строка в оригинальном файле не пустая, а в новом пустая
+                    lines.add(new LineItem(Type.REMOVED, originFile.get(i)));
+                    
+               }else if(originFile.get(i).isEmpty() && !editedFile.get(j).isEmpty()){
+                //если строка в оригинальном файле пустая, а в новом не пустая
+                    lines.add(new LineItem(Type.ADDED, editedFile.get(j)));
+                }
+                i++;j++;
+            } 
+
+           if(j < editedFile.size()){
+                lines.add(new LineItem(Type.ADDED, editedFile.get(j)));
+               j++;
+           }
+
+           if(i < originFile.size()){
+                lines.add(new LineItem(Type.REMOVED, originFile.get(i)));
+                j++;
+            }
 
             
 
-            //test
             lines.forEach((o) -> System.out.println(o.type + " " + o.line));
         }catch(IOException ioe){
             ioe.printStackTrace();
         }
 
     }
-
-
 
 
     public static enum Type {
